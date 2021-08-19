80046b382439bd211a5dcb8e04edd98776a92297
Erick Kleniving <libertti@live.com>
2021-08-19 14:37:36 -0300

    Sistema de star rating

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ class StarRater extends HTMLElement{
 }
 
 // É obrigatorio a tag ter "-" no nome,
-customElements.define('star-rater', StarRater);
+customElements.define('star-rater', StarRater); // define star-rater como elemento
 ~~~
 
 ~~~html
@@ -18,4 +18,53 @@ customElements.define('star-rater', StarRater);
   <star-rater></star-rater>
   <script src="StarRater.js"></script>
 <body>
+~~~
+
+Basicamento podemos utilizar essa estrutura para criar Web Components com Shadow dom
+
+~~~Javascript
+
+build(){  // escopo de build
+    const shadow = this.attachShadow({mode: 'open'})
+    shadow.appendChild(this.styles())
+
+    const rater = this.createRater();
+    const stars = this.createStars();
+
+    stars.forEach( star =>{
+      rater.appendChild(star)
+    })
+    shadow.appendChild(rater)
+  } //build
+  createRater(){ // Elemento pai de span
+    const rater = document.createElement('div');
+    rater.classList.add('star-rater')
+    return rater;
+  } // createRater
+
+  createStars(){ // Elemento span filho -  star
+    const createStar = (_, id) =>{
+      const star = document.createElement('span')
+      star.classList.add('star');
+      star.setAttribute('data-value', Number(id) +1)
+      star.innerHTML = '&#9733;'
+      return star;
+    }
+    return Array.from({length: 5}, createStar);
+  } // createStars
+
+  styles(){// Css do elemento
+    const style = document.createElement('style');
+    style.textContent =`
+    .star-rater {
+      font-size: 25px;
+      background-color:#0f0;
+    }
+    `
+    return style
+  } // styles
+
+
+}
+
 ~~~
--- Star-rater/StarRater.js
+++ Star-rater/StarRater.js
@@ -10,38 +10,72 @@ class StarRater extends HTMLElement{
     shadow.appendChild(this.styles())
 
     const rater = this.createRater();
-    const stars = this.createStars();
+    this.stars = this.createStars();
+    this.resetStars()
 
-    stars.forEach( star =>{
+    this.stars.forEach( star =>{
       rater.appendChild(star)
     })
     shadow.appendChild(rater)
-  }
+  } //build
+
   createRater(){
     const rater = document.createElement('div');
     rater.classList.add('star-rater')
     return rater;
-  }
+  } // createRater
+
   createStars(){
     const createStar = (_, id) =>{
       const star = document.createElement('span')
       star.classList.add('star');
       star.setAttribute('data-value', Number(id) +1)
       star.innerHTML = '&#9733;'
+
+      // Adicionado evento de click , chamando a função setRaging passando o this do obj StarRater
+      star.addEventListener('click', this.setRating.bind(this))
+      star.addEventListener('mouseover', this.hatingHover.bind(this))
+      star.addEventListener('mouseout', this.resetStars.bind(this))
+
       return star;
     }
     return Array.from({length: 5}, createStar);
+  } // createStars
+
+  resetStars(){
+    this.currentRatingValue = this.getAttribute('data-rating') || 0
+    this.hightlightRating()
+  }
+  setRating(event){
+    // ReferenciaObj.setandoAtributo('data-rating',evento.quemDisparouEvento.pegueOAtributo('data-value'))
+    // event.currentTarget.getAttribute('data-value') retorna oque esta dentro do Atributo 'data-value'
+    this.setAttribute('data-rating', event.currentTarget.getAttribute('data-value'))
+  } // setRating
+
+  hatingHover(event){
+    this.currentRatingValue = event.currentTarget.getAttribute('data-value')// recebe o data-value da estrela que o mouse estiver em cima
+    this.hightlightRating()
+
+  } // hatingHover
+
+  hightlightRating(){
+    this.stars.forEach(star =>{
+      star.style.color = this.currentRatingValue >= star.getAttribute('data-value') ? 'yellow' : 'gray'
+    })
+    
   }
 
   styles(){
     const style = document.createElement('style');
     style.textContent =`
-    .star-rater {
-      background-color:#0f0;
+    .star{
+      font-size: 5rem;
+      color: gray;
+      cursor: pointer;
     }
     `
     return style
-  }
+  } // styles
 
 
 }
